2d5c3e29fa252f51052263422ea86ed226cd1462
Abhilash <abhi2001jnv@gmail.com>
2025-05-26 12:24:21 +0530

    Added Navbar and Home page section with scroll animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroComponent } from './components/hero/hero.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  imports: [NavbarComponent, HeroComponent]
 })
 export class AppComponent implements OnInit {
   ngOnInit(): void {
